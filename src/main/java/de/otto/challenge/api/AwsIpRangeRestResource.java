f38ffd517c5daf27bd8a037bf5ae3c07595cb38f
Mathias Arens <mathias@arens-festers.de>
2023-06-10 22:01:34 +0200

    Trying to add aws sam deployment

--- src/main/java/de/otto/challenge/api/AwsIpRangeRestResource.java
+++ src/main/java/de/otto/challenge/api/AwsIpRangeRestResource.java
@@ -1,10 +1,7 @@
 package de.otto.challenge.api;
 
 import de.otto.challenge.domain.AwsIpRangeDto;
-import de.otto.challenge.domain.AwsIpRangesDto;
 import de.otto.challenge.service.AwsIpRangeService;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -25,7 +22,7 @@ public class AwsIpRangeRestResource {
         this.awsIpRangeService = awsIpRangeService;
     }
 
-    @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+    @GetMapping(value ="awsIpRange", produces = MediaType.TEXT_PLAIN_VALUE)
     public String awsIpRange(@RequestParam(value = "region", required = false) @Pattern(regexp = "(EU|US|AP|CN|SA|AF|CA|ALL)?", message = "Valid regions are EU|US|AP|CN|SA|AF|CA|ALL") String region) {
         final List<AwsIpRangeDto> awsIpRangeDtos = awsIpRangeService.loadAwsIpRangeForRegion(region);
         return convertAwsIpRangeDtosToString(awsIpRangeDtos);
@@ -34,8 +31,8 @@ public class AwsIpRangeRestResource {
     private String convertAwsIpRangeDtosToString(final List<AwsIpRangeDto> awsIpRangeDtos){
         final StringBuilder resultBuilder = new StringBuilder();
         for (final AwsIpRangeDto awsIpRangeDto : awsIpRangeDtos) {
-            resultBuilder.append("Region: " + awsIpRangeDto.region() + "; ");
-            resultBuilder.append("IpPrefix: " + awsIpRangeDto.ipPrefix() + ";");
+            resultBuilder.append("Region: ").append(awsIpRangeDto.region()).append("; ");
+            resultBuilder.append("IpPrefix: ").append(awsIpRangeDto.ipPrefix()).append(";");
             resultBuilder.append(System.getProperty("line.separator"));
         }
         return resultBuilder.toString();
---
+++ src/main/java/de/otto/challenge/aws/lambda/handler/AsynchronousLambdaHandler.java
@@ -0,0 +1,27 @@
+package de.otto.challenge.aws.lambda.handler;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import jakarta.ws.rs.core.Application;
+import com.amazonaws.services.lambda.runtime.Context;
+
+public class AsynchronousLambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
+    private final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    public AsynchronousLambdaHandler() throws ContainerInitializationException {
+        handler = (SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse>)
+                new SpringBootProxyHandlerBuilder()
+                        .springBootApplication(Application.class)
+                        .asyncInit()
+                        .buildAndInitialize();
+    }
+
+    @Override
+    public AwsProxyResponse handleRequest(AwsProxyRequest input, Context context) {
+        return handler.proxy(input, context);
+    }
+}
